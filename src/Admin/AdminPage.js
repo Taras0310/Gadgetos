4ef0e0b714e1e6f6eeed20d4096b95f467757b2f
Taras0310 <taras03101999@ukr.net>
2021-09-08 23:11:36 +0300

    first commit

--- src/Admin/AdminPage.js
+++ src/Admin/AdminPage.js
@@ -63,69 +63,12 @@ export default function AdminPage(){
         setOpen(false); 
       };
       
-    //   const addProduct= ()=>{
-    //     db.collection('Goods').add({ 
-    //         description: descriptionRef.current.value, 
-    //         category: categoryRef.current.value, 
-    //         quantity: quantityRef.current.value, 
-    //         price: priceRef.current.value, 
-    //         img: imgRef.current.value 
-    //     })
-        
-    //   }
-
-    //   const editTodo = () => { 
-    //     db.collection('Goods').doc(toUpdateId).update({ 
-    //         description: updateDescriptionRef.current.value, 
-    //         category: updateCategoryRef.current.value, 
-    //         quantity: updateQuantityRef.current.value, 
-    //         price: updatePriceRef.current.value, 
-    //         img: updateImgRef.current.value 
-    //     }); 
-    //     setOpen(false); 
-    //   }
-
-
+   
 
     return (
         <div className="admin-content">
 
-{/* <form className="bookForm" onSubmit={submit}> 
- 
- <div className="formCont"> 
 
-     <div className="inputCont"> 
-         <label for="category">Enter category</label> 
-         <input className="bookInputs" type="text" id="category" ref={categoryRef}/> 
-     </div> 
-     <div className="inputCont"> 
-         <label for="description">Enter description</label> 
-         <input className="bookInputs" type="text" id="description" ref={descriptionRef}/> 
-     </div> 
-
- </div> 
-
- <div className="formCont"> 
-
-     <div className="inputCont"> 
-         <label for="quantity">Enter quantity</label> 
-         <input className="bookInputs" type="text" id="quantity" ref={quantityRef}/> 
-     </div> 
-     <div className="inputCont"> 
-         <label for="price">price</label> 
-         <input className="bookInputs" type="text" id="price" ref={priceRef}/> 
-     </div> 
-
-   
- </div> 
- <div className="inputCont"> 
-         <label for="image">image</label> 
-         <input className="bookInputs" type="text" id="image" ref={imgRef}/> 
-     </div> 
-
- 
-  
-</form> */}
          
           <table border='1'> 
                 <tr> 
@@ -189,52 +132,6 @@ export default function AdminPage(){
              editable={editable}
             />
 
-            {/* <Dialog open={open} onClose={handleClose}> 
-          <form className="editForm"> 
- 
-          <div className="formContEdit"> 
- 
-          <div className="inputCont"> 
-                        <label for="category">Update category</label> 
-                        <input className="bookInputs" type="text" id="category" ref={updateCategoryRef}/> 
-                    </div> 
-                    <div className="inputCont"> 
-                        <label for="description">Update description</label> 
-                        <input className="bookInputs" type="text" id="description" ref={updateDescriptionRef}/> 
-                    </div> 
- 
- 
-            <div className="inputCont"> 
-                <label for="quantity">Update quantity</label> 
-                <input className="bookInputs" type="text" id="quantity" ref={updateQuantityRef}/> 
-            </div> 
-            <div className="inputCont"> 
-                <label for="price">Update price</label> 
-                <input className="bookInputs" type="text" id="price" ref={updatePriceRef}/> 
-            </div> 
- 
- 
-            <div className="inputCont"> 
-                        <label for="price">Update image</label> 
-                        <input className="bookInputs" type="text" id="price" ref={updateImgRef}/> 
-                    </div> 
- 
- 
-            </div> 
- 
-            <Button onClick={handleClose} color="primary"> 
-            Cancel 
-          </Button> 
- 
-          <Button onClick={editTodo} color="primary"> 
-            Save 
-          </Button> 
-       
-            </form> 
-           
-         
-      </Dialog> */}
-           
         </div>
     )
 }
